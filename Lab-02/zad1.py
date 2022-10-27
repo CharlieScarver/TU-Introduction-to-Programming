33479a7c4a8362bbcdc5bafa538a688456050a4d
CharlieScarver <s.apostolovd@gmail.com>
2022-10-27 16:05:12 +0300

    Adding code from Lab-02

---
+++ Lab-02/zad1.py
@@ -0,0 +1,20 @@
+#a = int(input("Enter an integer number: "))
+#b = int(input("Enter a numerical system base: "))
+
+a = int(input("Enter an integer number: "))
+b = int(input("Enter another integer number: "))
+
+if b == 2:
+    a = 2
+    print('b is 2')
+elif b == 2:
+    a = 3
+    print('b is 3')
+elif b == 4:
+    a = 4
+    print('b is 4')
+else:
+    a = 999999
+    print('b is something else')
+
+print(a)
