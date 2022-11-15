1c5572dc7367ab9ee21437e1e9cd3baf4a6d0655
CharlieScarver <s.apostolovd@gmail.com>
2022-11-15 03:16:37 +0200

    Adding code written during class for Lab-04

---
+++ Lab-04/lab-04-in-class.py
@@ -0,0 +1,49 @@
+matrix = [
+    [1,2,3],
+    [2,3,4],
+    [3,4,5]
+]
+
+if 1 in matrix[1]:
+    print("yes")
+else:
+    print("no")
+
+matrix[0][1]
+
+
+s = [1, 2, 3, 4, 5, 6]
+for e in s:
+    print(e, end=' ')
+
+print()
+
+for i, e in enumerate(s):
+    print(i, e)
+
+
+ss = "hello"
+if "e" in ss or "l" in ss:
+    print("yes")
+
+import random
+
+
+s = []
+s.insert(0, 90)
+
+dayOfTheWeekend = ("Saturday", "Sunday")
+
+
+day = dayOfTheWeekend[0]
+
+d = {
+    'name': 'ivan',
+    'last_name': 'ivanov'
+}
+print(d['name'])
+print('asd' in d)
+
+
+
+
