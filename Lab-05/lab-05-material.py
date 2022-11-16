45a27619043aa63067ab3494cc8afd3360b919e2
CharlieScarver <s.apostolovd@gmail.com>
2022-11-16 03:54:07 +0200

    Adding more explainer code for Namespaces and Variable Scope for Lab-05

--- Lab-05/lab-05-material.py
+++ Lab-05/lab-05-material.py
@@ -1,3 +1,14 @@
+# What type is this?
+
+a = 5,
+b = {}
+c = range(10)
+d = [a]
+e = [x for x in c]
+
+
+##########
+
 # Functions
 
 # print(), input(), int(), list(), type()
@@ -72,36 +83,86 @@ def primes_in_interval(start, end): # end is inclusive
 primes_in_interval(25, 50)
 print("########")
 
-############
-# Pass by value vs pass by reference
+###########
+# In Python we have objects and names referencing them
+# Every objects stores a count - the number of references
 
-num = 5
+# Sequence Types
+#       - x in s, s + t, s * n
+#       - s[i], s[i:j:k], len(s), max(s), s.index(x), s.count(x)
 
-def modify_num(num):
-    num += 2
+## Immutable Sequence Types
+#       - lists, sets, dictionaries
+#       - s[i] = x, del s[i:j], s[i:j:k] = t
+#       - s.append() s.copy(), s.clear(), s.insert(i, x), s.pop(), s.remove()
+#       - can be changed in functions, can't be reassigned
 
-print(num)
-modify_num(num)
-print(num)
+## Mutable Sequence Types
+#       - strings, tuples, ranges
+#       - hash()
 
-s = "Hello"
-def modify_str(s):
-    s.lower()
+###########
 
-print(s)
-modify_str(s)
-print(s)
+# Argument passing, references and rebinding
 
-lst = [43, 2, 17, 95]
+# Recall that in Python, every piece of data is an object.
+# A reference points to an object, not a specific memory location.
 
-def modify_list(lst):
-    lst.sort()
-    lst = "new value"
+x = 5
+x = 10
 
-print(lst)
-modify_list(lst)
-print(lst)
+# These assignment statements have the following meaning:
 
+# The first statement causes x to point to an object whose value is 5.
+
+# The next statement reassigns x as a new reference to a different
+# object whose value is 10. Stated another way, the second assignment
+# rebinds x to a different object with value 10.
+
+# In Python, when you pass an argument to a function, a similar
+# rebinding occurs. Consider this example:
+
+def f(fx):
+    fx = 10
+
+x = 5
+f(x)
+print(x)    # 5
+
+# Namespaces
+# Local > Enclosing > Global (Module) > Built-in
+
+dir()
+dir(__builtins__)
+globals()
+locals()
+
+# Variable scope
+print("Variable scope:")
+
+x = 'global'
+
+def f():
+    x = 'local'
+
+f()
+print(x)    # global
+
+# Namespace resolution
+# Local > Enclosing > Global > Built-in
+print("Namespace resolution:")
+
+###
+
+name = "Alex"
+
+def foo():
+    print(f"{name}")
+
+foo()
+
+
+###
 
 def foo():
     name = "Alex"
@@ -113,6 +174,101 @@ def foo():
 
 foo()
 
+###
+
+x = 'global'
+
+def f():
+    x = 'enclosing'
+
+    def g():
+        x = 'local'
+        print(x) # local
+
+    g()
+    print(x) # enclosing
+
+f()
+print(x)    # global
+
+#
+# Modifying global names in a function
+print("Modifying global:")
+
+x = 'global'
+
+def f():
+    global x
+    x = 'local'
+
+f()
+print(x)    # local
+
+#
+# Modifying enclosing names in a function
+print("Modifying enclosing:")
+
+x = 'global'
+
+def f():
+    x = 'enclosing'
+
+    def g():
+        nonlocal x
+        x = 'local'
+        print(x) # local
+
+    g()
+    print(x) # local
+
+f()
+print(x)    # global
+
+############
+
+# Pass by value vs pass by reference
+print("Modify function arguments:")
+
+# Number
+num = 5
+
+def modify_num(num):
+    print(num, id(num))
+    num += 2
+    print(num, id(num))
+
+print(num, id(num))
+modify_num(num)
+print(num, id(num))
+
+# String
+
+s = "Hello"
+def modify_str(s):
+    s.lower()
+    print(s, id(s))
+    s = s.lower()
+    print(s, id(s))
+
+print(s, id(s))
+modify_str(s)
+print(s, id(s))
+
+# List
+
+lst = [43, 2, 17, 95]
+
+def modify_list(lst):
+    lst.sort()
+    print(lst, id(lst))
+    lst = [43, 2, 17, 95]
+    print(lst, id(lst))
+
+print(lst, id(lst))
+modify_list(lst)
+print(lst, id(lst))
+
+
 # Reference count
 
 import sys
@@ -121,11 +277,6 @@ print(sys.getrefcount(s)) #-- Returns the reference count of the object.
 
 import gc
 
-print(len(gc.get_referrers(s)))
-for i in gc.get_referrers(s):
-    print(i)
-
-# In Python we have objects and names referencing them
-# Every objects stores a count - the number of references
-
-
+print("referrers count: ", len(gc.get_referrers(s)))
+# for i in gc.get_referrers(s):
+#    print(i)
