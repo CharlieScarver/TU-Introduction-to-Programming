0355e992a12c8b179f99fbda89a8ccf4589ad707
CharlieScarver <s.apostolovd@gmail.com>
2022-12-01 19:50:53 +0200

    Added Polymorphism mini example

--- Lab-06/lab-06-4.py
+++ Lab-06/lab-06-4.py
@@ -9,3 +9,23 @@ class B(A):
 
 
 b = B(1, 2)
+
+#####
+
+class Animal:
+    pass
+
+class Duck(Animal):
+    pass
+ 
+a = Animal()
+b = Duck()
+
+# isinstance(obj, class)
+# Returns if obj is an object of that class
+
+isinstance(5, int)      # True
+isinstance(a, Animal)   # True
+isinstance(b, Duck)     # True
+isinstance(b, Animal)   # True!! due to Polymorphism
+
